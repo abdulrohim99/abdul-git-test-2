5e240b2526abc19db264728891b61c910b14775b
abdulrohim99 <the26abd@gmail.com>
2019-08-09 18:30:04 -0700

    menambahkan list baru

--- index.php
+++ index.php
@@ -11,6 +11,11 @@
 <body>
     <h1> Hello World</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem vero eum corporis vitae aliquam consequatur ab sint iste, quod facilis minima quasi eveniet? Molestiae nostrum, nisi molestias eaque magnam consectetur.</p>
+
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
